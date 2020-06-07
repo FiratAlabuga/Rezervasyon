edb70df7522185ba20f224d20cf7147a74e646fb
unknown <jfirat.alabuga@gmail.com>
2020-06-07 16:24:14 +0300

    C# ve MSSQL

---
+++ Rezervasyon/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Maas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oğulcan Kaya");
+            Console.WriteLine("G-180100080");
+
+            int ID;
+            Console.WriteLine("Personel ID Giriniz");
+            List<int> PersonelList = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                ID = int.Parse(Console.ReadLine());
+                PersonelList.Add(ID);  
+            }
+
+            int aranaIndex = MaasZammi(PersonelList.IndexOf(2));
+            Console.WriteLine("Maaşa Zam Yapılmıştır :"+aranaIndex);
+
+            static int MaasZammi(int id)
+            {
+
+                int MaasBaslangic = 100;
+
+                switch (id)
+                {
+                    case 1:
+                        MaasBaslangic += 10;
+                        
+                        goto case 2; 
+                    case 2:
+                        return MaasBaslangic * 10;
+                    default:
+                        return MaasBaslangic;
+                }
+            }
+        }
+    }
+}
